3156dbbbbe535628915c2241de1afcb07ac4746f
CalebDH31 <calebdh31@gmail.com>
2025-08-07 21:57:33 -0600

    lecture finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as weatherService from "./services/weatherService"
+import WeatherSearch from './components/WeatherSearch/WeatherSearch' 
+import WeatherDetails from './components/WeatherDetails/WeatherDetails'
 import './App.css'
 
 // src/App.jsx
 
 function App() {
+  const [weather, setWeather] = useState({})
+
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+    console.log('Data:', data);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+};
+setWeather(newWeatherState)
+  };
 
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <WeatherSearch fetchData={fetchData}/>
+      <WeatherDetails weather={weather}/>
+    </main>
   );
-}
+};
 
 export default App
 
---
+++ src/services/weatherService.js
@@ -0,0 +1,17 @@
+const API_KEY ="7f82365af97244fcb1b31454250808"
+const BASE_URL =`http://api.weatherapi.com/v1/current.json?key=7f82365af97244fcb1b31454250808`
+
+const show = async (city) => {
+    try {
+        const queryString = `&q=${city}`
+        const res = await fetch(BASE_URL + queryString)
+        const data = await res.json()
+        console.log('Data:', data)
+        return data
+    } catch (err) {
+        console.log(err)
+    }
+}
+export {
+show,
+}
